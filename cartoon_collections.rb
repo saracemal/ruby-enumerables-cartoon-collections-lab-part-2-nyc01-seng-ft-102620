47f0e6295614875d9f31677b6334ed75037970fa
Sara Cemal <saracemal@gmail.com>
2020-10-21 04:08:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]  
   
-  planeteer_calls.include? "Earth!"
+  planeteer_calls.include? { |call| valid_calls.find }
   
 end
