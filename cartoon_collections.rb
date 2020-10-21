3b0cedc4912ab82139d008912dd335251bd5afec
Sara Cemal <saracemal@gmail.com>
2020-10-21 04:03:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,14 +14,13 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls. do |element|
-    element >= 4
+  planeteer_calls.any? { |call| call.length > 4}
 end
 end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.find { |element| element.
+  planeteer_calls.find { |call| element.
   
 end
