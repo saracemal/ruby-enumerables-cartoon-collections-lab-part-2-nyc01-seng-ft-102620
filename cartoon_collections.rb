e14a49a43ed438c65c59cc06f61d003f50f02b02
Sara Cemal <saracemal@gmail.com>
2020-10-21 04:06:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]  
   
-  planeteer_calls.include? valid_calls
+  planeteer_calls.include? 
   
 end
