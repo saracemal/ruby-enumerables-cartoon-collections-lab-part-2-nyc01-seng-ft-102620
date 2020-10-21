fdc7e5ab5b523dd9d4c03e1335d1e85d49aab1bc
Sara Cemal <saracemal@gmail.com>
2020-10-21 04:05:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_valid_calls(planeteer_calls)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]  
   
+  planeteer_calls.
   
 end
