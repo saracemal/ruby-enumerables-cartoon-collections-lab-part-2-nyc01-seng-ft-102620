5cd0dbec999119d7c5d4fb4e9587bbc4a1d22cdd
Sara Cemal <saracemal@gmail.com>
2020-10-21 04:01:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |element|
+  planeteer_calls. do |element|
     element >= 4
 end
 end
