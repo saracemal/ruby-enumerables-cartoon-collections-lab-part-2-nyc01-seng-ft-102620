e718ad63ea825ac5ed0f72644280b2b9f6cb2a82
Sara Cemal <saracemal@gmail.com>
2020-10-21 03:00:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,12 +2,9 @@ def square_array(array)
   new_array = []
   index = 0
   
-  while index < array.length do 
-    new_array << array[index] * array[index]
-      index += 1
+  array.map do |element| 
+    new_array << element * element
     end
-    
-  new_array
 end
 
 def summon_captain_planet(planeteer_calls)
